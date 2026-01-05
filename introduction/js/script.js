394d7f20b908125ca79afae2887301018f33641e
Ma. Sofia Anne Revilla <mcrevilla@student.apc.edu.ph>
2026-01-06 07:58:29 +0800

    attempt to add more effect and light/darkmode-aslo change the foldername of css

--- introduction/js/script.js
+++ introduction/js/script.js
@@ -1,5 +1,7 @@
 //try adding java for effects (Inspired from the internet)
 
+/// --- LIGHTBOX FUNCTIONALITY ---
+
 // Get elements
 const lightbox = document.getElementById('lightbox');
 const lightboxImg = document.getElementById('lightbox-img');
@@ -27,5 +29,26 @@ closeBtn.addEventListener('click', () => {
 lightbox.addEventListener('click', (e) => {
     if (e.target !== lightboxImg) {
         lightbox.style.display = "none";
+    }
+});
+
+
+// --- DARK MODE TOGGLE ---
+
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = themeToggle.querySelector('i');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+    // 1. Toggle the class on the body
+    body.classList.toggle('dark-mode');
+
+    // 2. Switch the Icon (Moon <-> Sun)
+    if(body.classList.contains('dark-mode')) {
+        themeIcon.classList.remove('bi-moon');
+        themeIcon.classList.add('bi-sun');
+    } else {
+        themeIcon.classList.remove('bi-sun');
+        themeIcon.classList.add('bi-moon');
     }
 });
